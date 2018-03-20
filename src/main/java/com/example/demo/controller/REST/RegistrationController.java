e484ddfd4f4d859a012176021a9a8ea686b04e3d
azhulinski <a.zhulinski@gmail.com>
2018-03-20 20:31:01 +0200

    fixed registration controller

--- src/main/java/com/example/demo/controller/REST/RegistrationController.java
+++ src/main/java/com/example/demo/controller/REST/RegistrationController.java
@@ -1,8 +1,8 @@
 package com.example.demo.controller.REST;
 
 /*
-* REST Controller of registration form.
-* */
+ * REST Controller of registration form.
+ * */
 
 import com.example.demo.entity.User;
 import com.example.demo.service.UserService;
@@ -21,17 +21,7 @@ public class RegistrationController {
     @PostMapping("/saveUser") // this POST method reacts to post type from $.ajax
     public void saveUser(@RequestBody User user) {
 
-        // building new user with build() method from entity class(with @builder annotation)
-
-        User savedUser = User
-                .builder()
-                .login(user.getLogin())
-                .password(user.getPassword())
-                .firstName(user.getFirstName())
-                .lastName(user.getLastName())
-                .build();
-
         //saving user to database with save method from Service layer
-        userService.save(savedUser);
+        userService.save(user);
     }
 }
